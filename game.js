932cc07fc9a50cbc99215185e4eeb50276e880db
Bilal ayachi <bilal.ayachi@hetmml.nl>
2017-10-16 20:15:49 +0200

    Constructor functie toegepast

---
+++ game.js
@@ -0,0 +1,29 @@
+var slang;
+
+
+function setup() {
+  createCanvas(800,600);
+  slang = new Snake();
+
+}
+
+function keyPressed() {
+  if (keyCode == UP_ARROW) {
+    slang.dir(0,-1);
+  } else if (keyCode == DOWN_ARROW) {
+      slang.dir(0,1);
+    } else if (keyCode == RIGHT_ARROW) {
+        slang.dir(1,0);
+    } else if (keyCode == LEFT_ARROW) {
+        slang.dir(-1,0);
+    }
+}
+
+
+
+function draw() {
+    background(50);
+    slang.update();
+    slang.show();
+
+}
